f9fa87beb63690c54c3e72713505abfc663dbf36
yanbhliu <55248573+yanbhliu@users.noreply.github.com>
2022-04-10 01:02:34 -0400

    CheckDynamic

--- include/yolo.h
+++ include/yolo.h
@@ -26,15 +26,17 @@ namespace yolov3 {
             int inpWidth = 640;  // Width of network's input image
             int inpHeight = 480; // Height of network's input image
             vector<string> classes;
+            // vector<Point2f> mvpt;
             Net net;
 
         public:
             yolov3Segment();
             ~yolov3Segment();
-            cv::Mat Segmentation(cv::Mat &image);
+            cv::Mat Segmentation(cv::Mat &image, cv::Mat &image2);
+            vector<vector<float>> opticalFlowDetect(cv::Mat& img1, cv::Mat& img2);
 
             // Remove the bounding boxes with low confidence using non-maxima suppression
-            cv::Mat postprocess(Mat& frame, const vector<Mat>& out);
+            cv::Mat postprocess(Mat& frame,Mat& frame2, const vector<Mat>& out);
             // Get the names of the output layers
             vector<String> getOutputsNames(const Net& net);
 
