d5bf35694c50371ee1ba4697e5049255d32e4141
yanbhliu <55248573+yanbhliu@users.noreply.github.com>
2022-04-20 21:25:38 -0400

    plot of trajectory

--- matlab/Trajectory_plotter.m
+++ matlab/Trajectory_plotter.m
@@ -1,9 +1,11 @@
-%% load data
+close all
+clear
+clc
+
+% ```modified the file name and path to your trajectories.
 
 % ground truth
-% change dir
-%fileID = fopen('groundtruth_halfsphere.txt','r');
-fileID = fopen('groundtruth_xyz.txt','r');
+fileID = fopen('sitting_groundtruth.txt','r');
 formatSpec = '%f %f %f %f %f %f %f %f';
 sizeA = [8 Inf];
 traj_gt = fscanf(fileID,formatSpec,sizeA);
@@ -11,49 +13,47 @@ fclose(fileID);
 traj_gt = traj_gt';
 traj_gt(:, 2:4) = traj_gt(:, 2:4) - traj_gt(1, 2:4);
 
-% orb_slam
-%fileID = fopen('KeyFrameTrajectory_halfsphere.txt','r');
-fileID = fopen('KeyFrameTrajectory_rgbd_xyz3.txt','r');
+% DynaSLAM: Mask RCNN
+fileID = fopen('KeyFrameTrajectory_maskRCNN_sitting.txt','r');
 formatSpec = '%f %f %f %f %f %f %f %f';
 sizeA = [8 Inf];
 traj = fscanf(fileID,formatSpec,sizeA);
 fclose(fileID);
 traj = traj';
 
-% dynaslam with yolo
-%fileID = fopen('KeyFrameTrajectory_halfsphere_yolo.txt','r');
-fileID = fopen('KeyFrameTrajectory_rgbd_xyz_yolo.txt','r');
+% DOT SLAM
+fileID = fopen("KeyFrameTrajectory_DOT_sitting.txt","r");
 formatSpec = '%f %f %f %f %f %f %f %f';
 sizeA = [8 Inf];
-traj_yolo = fscanf(fileID,formatSpec,sizeA);
+traj_yolo_DOT = fscanf(fileID,formatSpec,sizeA);
 fclose(fileID);
-traj_yolo = traj_yolo';
+traj_yolo_DOT = traj_yolo_DOT';
 
-%% plot
-%%{
+% ORB_SLAM2
+fileID = fopen('KeyFrameTrajectory_orbSLAM_sitting.txt','r');
+formatSpec = '%f %f %f %f %f %f %f %f';
+sizeA = [8 Inf];
+traj_orb = fscanf(fileID,formatSpec,sizeA);
+fclose(fileID);
+traj_orb = traj_orb';
+
+%% plot trajectories
 figure(1);
-plot3(traj_gt(:,2), traj_gt(:,3), traj_gt(:,4), 'linewidth', 1.5);
+plot3(traj_gt(:,2), traj_gt(:,3), traj_gt(:,4), 'linewidth', 1.5,'Color','#0072BD');
 hold on;
-plot3(traj(:,2), traj(:,3), traj(:,4), 'linewidth', 1.5);
-hold on;
-plot3(traj_yolo(:,2), traj_yolo(:,3), traj_yolo(:,4), 'linewidth', 1.5);
+plot3(traj(:,2), traj(:,3), traj(:,4), 'linewidth', 1.5,'Color','#EDB120');
+
+plot3(traj_yolo_DOT(:,2), traj_yolo_DOT(:,3), traj_yolo_DOT(:,4), 'linewidth', 1.5, 'Color','#77AC30');
+
+plot3(traj_orb(:,2), traj_orb(:,3), traj_orb(:,4), 'linewidth', 1.5,'Color','r');
+
 axis auto, grid on
 xlabel('$X$', 'Interpreter', 'latex')
 ylabel('$Y$', 'Interpreter', 'latex')
 zlabel('$Z$', 'Interpreter', 'latex')
-legend('$Ground\ truth$', '$ORB\_SLAM$', '$YOLO\ Dynamic\ ORB\_SLAM$', 'Interpreter', 'latex', 'location', 'best')
-view(-50,34)
-%%}
+title("sitting\_xyz")
 
-% if use sensor fusion toolbox
-%{
-tp = theaterPlot('XLimit',[-2 2],'YLimit',[-2 2],'ZLimit',[-0.5 2]);
-op = orientationPlotter(tp,'DisplayName','Fused Data',...
-    'LocalAxesLength',2);
-for i=1:size(traj_gt, 1)
-    %plotOrientation(op, quaternion(traj_yolo(i,5),traj_yolo(i,6),traj_yolo(i,7),traj_yolo(i,8)));
-    plotOrientation(op, quaternion(traj_gt(i,5),traj_gt(i,6),traj_gt(i,7),traj_gt(i,8)), [traj_gt(i,2) traj_gt(i,3) traj_gt(i,4)]);
-    drawnow
-    %pause(0.1);
-end
-%}
+legend('$Ground\ truth$', '$Dyna\_SLAM$', '$DOT\_SLAM$', '$ORB\_SLAM2$','Interpreter', 'latex', 'location', 'best')
+view(-50,35) % uncomment to show 3D image
+% view(90,-90) % uncomment to show 2D image of x,y axis.
+
